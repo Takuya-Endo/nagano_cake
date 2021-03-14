7f23eb65bdaeab9dc6ab73cc340ae2053fbed545
Takuya-Endo <above5purple@gmail.com>
2021-03-14 12:25:47 +0000

    [Add]Adminモデル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210314121534) do
+ActiveRecord::Schema.define(version: 20210314122313) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer  "customer_id", null: false
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20210314121534) do
     t.string   "address",     null: false
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "admins", force: :cascade do |t|
+    t.string   "email",              null: false
+    t.string   "encrypted_password", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
   end
 
   create_table "customers", force: :cascade do |t|
