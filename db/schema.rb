d6730bb66fc6c13ce39da15c0f27c15fa3caec3b
Takuya-Endo <above5purple@gmail.com>
2021-03-14 12:34:44 +0000

    [Add]Genreモデル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210314122827) do
+ActiveRecord::Schema.define(version: 20210314123319) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer  "customer_id", null: false
@@ -41,6 +41,12 @@ ActiveRecord::Schema.define(version: 20210314122827) do
     t.boolean  "is_active",          default: true, null: false
     t.datetime "created_at",                        null: false
     t.datetime "updated_at",                        null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "items", force: :cascade do |t|
