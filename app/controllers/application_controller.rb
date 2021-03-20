aafeab1e2d11b78d9e07b07bb08b8d81f5187506
Takuya-Endo <above5purple@gmail.com>
2021-03-20 09:18:16 +0000

    [Add]new.html.erb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
 
-  before_action :authenticate_user!,except: [:top]
+  # before_action :authenticate_user!,except: [:top]
 
   protect_from_forgery with: :exception
 end
