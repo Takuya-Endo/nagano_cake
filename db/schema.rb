59f99bb4501d9aee08a07dfd32b5f5e1484afb6d
Takuya-Endo <above5purple@gmail.com>
2021-03-20 00:53:57 +0000

    [Remove]customersテーブル、adminsテーブル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210314132923) do
+ActiveRecord::Schema.define(version: 20210320004923) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer  "customer_id", null: false
@@ -21,34 +21,12 @@ ActiveRecord::Schema.define(version: 20210314132923) do
     t.datetime "updated_at",  null: false
   end
 
-  create_table "admins", force: :cascade do |t|
-    t.string   "email",              null: false
-    t.string   "encrypted_password", null: false
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
-  end
-
   create_table "cart_items", force: :cascade do |t|
     t.integer  "item_id",     null: false
     t.integer  "customer_id", null: false
     t.integer  "amount",      null: false
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-  end
-
-  create_table "customers", force: :cascade do |t|
-    t.string   "last_name",                         null: false
-    t.string   "first_name",                        null: false
-    t.string   "last_name_kana",                    null: false
-    t.string   "first_name_kana",                   null: false
-    t.string   "email",                             null: false
-    t.string   "encrypted_password",                null: false
-    t.string   "postal_code",                       null: false
-    t.string   "address",                           null: false
-    t.string   "telephone_number",                  null: false
-    t.boolean  "is_active",          default: true, null: false
-    t.datetime "created_at",                        null: false
-    t.datetime "updated_at",                        null: false
   end
 
   create_table "genres", force: :cascade do |t|
