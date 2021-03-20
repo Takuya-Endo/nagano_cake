9e44788747db50a1938c88e8e7b395526e91e9f2
Takuya-Endo <above5purple@gmail.com>
2021-03-20 09:39:43 +0000

    [Add]createアクション、ルーティング

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -4,4 +4,15 @@ class Admin::ItemsController < ApplicationController
       @item = Item.new
     end
 
+    def create
+      item = Item.new(item_params)
+      item.save
+      redirect_to root_path
+    end
+
+    private
+      def item_params
+        params.require(:list).permit(:image, :name, :introduction, :price)
+      end
+
 end
