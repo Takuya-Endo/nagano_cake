cadbbd012ce6a336277ea9024711ee191743465f
Takuya-Endo <above5purple@gmail.com>
2021-03-28 02:34:35 +0000

    [Add]itemsコントローラのeditアクション、updateアクション

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -7,7 +7,7 @@ class Admin::ItemsController < ApplicationController
     def create
       item = Item.new(item_params)
       item.save
-      redirect_to "/admin/items/new"
+      redirect_to admin_item_path(item.id)
     end
 
     def index
@@ -16,6 +16,16 @@ class Admin::ItemsController < ApplicationController
 
     def show
       @item = Item.find(params[:id])
+    end
+
+    def edit
+      @item = Item.find(params[:id])
+    end
+
+    def update
+      item =Item.find(params[:id])
+      item.update(item_params)
+      redirect_to admin_item_path(item.id)
     end
 
     private
