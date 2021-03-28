5304b890688d522a7d2d791d82e14575febdc863
Takuya-Endo <above5purple@gmail.com>
2021-03-28 09:05:21 +0000

    [Update]Customerモデル、新規会員登録

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,11 @@ class ApplicationController < ActionController::Base
   # before_action :authenticate_customer!,except: []
 
   protect_from_forgery with: :exception
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :is_active])
+  end
+
 end
