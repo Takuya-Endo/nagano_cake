6d777bd6ccf095521106190076c6a850edd23b80
Takuya-Endo <above5purple@gmail.com>
2021-03-28 03:17:26 +0000

    [Add]Customerモデル

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
 
   before_action :authenticate_admin!,except: [:top]
+  # before_action :authenticate_customer!,except: []
 
   protect_from_forgery with: :exception
 end
