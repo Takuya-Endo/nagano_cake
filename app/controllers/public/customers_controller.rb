96ee86b585a783cce128b9a74a2149bb5a62d652
Takuya-Endo <above5purple@gmail.com>
2021-04-06 20:17:59 +0000

    [Add]edit,updateアクション、[Update]show.html.erb

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -5,11 +5,21 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(current_customer.id)
   end
 
   def update
+    customer = Customer.find(current_customer.id)
+    customer.update(customer_params)
+    redirect_to customer_path(current_customer.id)
   end
 
   def unsubscribe_confirm
   end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email)
+  end
+
 end
