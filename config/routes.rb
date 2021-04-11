bfa0081cbea3476a0b96bd6818dcb9c312e9eb98
Takuya-Endo <above5purple@gmail.com>
2021-04-11 16:02:20 +0000

    [Fix]devise_for :admin

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,50 @@ Rails.application.routes.draw do
 
   end
 
+
+#   scope module: :admin do
+    devise_for :admin, controllers: {
+    sessions: 'admins/sessions',
+    registrations: 'admins/registrations',
+    passwords: 'admins/passwords'
+    }
+#   end
+       #   new_admin_session GET    /admin/sign_in(.:format)       admin_sessions#new
+            #  admin_session POST   /admin/sign_in(.:format)       admin_sessions#create
+    #  destroy_admin_session DELETE /admin/sign_out(.:format)      admin_sessions#destroy
+        # new_admin_password GET    /admin/password/new(.:format)  admin_passwords#new
+     #   edit_admin_password GET    /admin/password/edit(.:format) admin_passwords#edit
+            # admin_password PATCH  /admin/password(.:format)      admin_passwords#update
+                         #   PUT    /admin/password(.:format)      admin_passwords#update
+                         #   POST   /admin/password(.:format)      admin_passwords#create
+#  cancel_admin_registration GET    /admin/cancel(.:format)        admin_registrations#cancel
+    # new_admin_registration GET    /admin/sign_up(.:format)       admin_registrations#new
+ #   edit_admin_registration GET    /admin/edit(.:format)          admin_registrations#edit
+        # admin_registration PATCH  /admin(.:format)               admin_registrations#update
+                         #   PUT    /admin(.:format)               admin_registrations#update
+                         #   DELETE /admin(.:format)               admin_registrations#destroy
+                         #   POST   /admin(.:format)               admin_registrations#create
+
+
+#   devise_for :admin
+        # new_admin_session GET    /admin/sign_in(.:format)       devise/sessions#new
+            # admin_session POST   /admin/sign_in(.:format)       devise/sessions#create
+    # destroy_admin_session DELETE /admin/sign_out(.:format)      devise/sessions#destroy
+       # new_admin_password GET    /admin/password/new(.:format)  devise/passwords#new
+      # edit_admin_password GET    /admin/password/edit(.:format) devise/passwords#edit
+           # admin_password PATCH  /admin/password(.:format)      devise/passwords#update
+                          # PUT    /admin/password(.:format)      devise/passwords#update
+                          # POST   /admin/password(.:format)      devise/passwords#create
+# cancel_admin_registration GET    /admin/cancel(.:format)        devise/registrations#cancel
+   # new_admin_registration GET    /admin/sign_up(.:format)       devise/registrations#new
+  # edit_admin_registration GET    /admin/edit(.:format)          devise/registrations#edit
+       # admin_registration PATCH  /admin(.:format)               devise/registrations#update
+                          # PUT    /admin(.:format)               devise/registrations#update
+                          # DELETE /admin(.:format)               devise/registrations#destroy
+                          # POST   /admin(.:format)               devise/registrations#create
+
   namespace :admin do
+
     root to: 'homes#top'
     resources :items, except: [:destroy]
               # admin_items GET    /admin/items(.:format)          admin/items#index
@@ -70,22 +113,6 @@ Rails.application.routes.draw do
 
   end
 
-  devise_for :admin
-        # new_admin_session GET    /admin/sign_in(.:format)       devise/sessions#new
-            # admin_session POST   /admin/sign_in(.:format)       devise/sessions#create
-    # destroy_admin_session DELETE /admin/sign_out(.:format)      devise/sessions#destroy
-       # new_admin_password GET    /admin/password/new(.:format)  devise/passwords#new
-      # edit_admin_password GET    /admin/password/edit(.:format) devise/passwords#edit
-           # admin_password PATCH  /admin/password(.:format)      devise/passwords#update
-                          # PUT    /admin/password(.:format)      devise/passwords#update
-                          # POST   /admin/password(.:format)      devise/passwords#create
-# cancel_admin_registration GET    /admin/cancel(.:format)        devise/registrations#cancel
-   # new_admin_registration GET    /admin/sign_up(.:format)       devise/registrations#new
-  # edit_admin_registration GET    /admin/edit(.:format)          devise/registrations#edit
-       # admin_registration PATCH  /admin(.:format)               devise/registrations#update
-                          # PUT    /admin(.:format)               devise/registrations#update
-                          # DELETE /admin(.:format)               devise/registrations#destroy
-                          # POST   /admin(.:format)               devise/registrations#create
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
