ce07fa4443755949ca17f0c6ff05e9b4f9fdd255
Takuya-Endo <above5purple@gmail.com>
2021-04-11 00:54:03 +0000

    [Add]kaminari,ページング機能、[Update]会員非ログイン時制限、ログイン時ヘッダー

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,5 +1,7 @@
 class Public::OrdersController < ApplicationController
 
+  before_action :authenticate_customer!
+
   def new
     @cart_items = CartItem.where(customer_id: current_customer.id)
     @items = Item.all
